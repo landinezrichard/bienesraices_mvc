ab322efd13a4225414e62595aabac011f96592ea
Ricardo <landinezrichard@gmail.com>
2025-10-24 20:51:03 -0500

    feat: formularios de login, registro y olvido de password

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,9 +1,19 @@
 const formularioLogin = (req, res) => {
-  res.render("auth/login");
+  res.render("auth/login", {
+    pagina: "Iniciar Sesión",
+  });
 };
 
 const formularioRegistro = (req, res) => {
-  res.render("auth/registro");
+  res.render("auth/registro", {
+    pagina: "Crear Cuenta",
+  });
 };
 
-export { formularioLogin, formularioRegistro };
+const formularioOlvidoPassword = (req, res) => {
+  res.render("auth/olvido-password", {
+    pagina: "Recupera tu acceso a Bienes Raíces",
+  });
+};
+
+export { formularioLogin, formularioRegistro, formularioOlvidoPassword };
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { formularioLogin, formularioRegistro } from "../controllers/usuarioController.js";
+import { formularioLogin, formularioRegistro, formularioOlvidoPassword } from "../controllers/usuarioController.js";
 
 const router = express.Router();
 
 router.get("/login", formularioLogin);
 router.get("/registro", formularioRegistro);
+router.get("/olvido-password", formularioOlvidoPassword);
 
 export default router; 
